ff3a2dc0b94accb54d198213167324e4d496fe0a
linwoodpendleton <linwood.pendleton@alumni.harvard.edu>
2024-11-05 19:28:47 +0800

    增加转发到代理Bug 30

--- src/forward/forward.rs
+++ src/forward/forward.rs
@@ -435,13 +435,13 @@ pub async fn handle_connection(
     let status_line = format!("HTTP/1.1 {} {}\r\n", response_code, status_text);
 
     // 合并所有部分，并确保有一个空行分隔头部和体
-    let full_response = format!(
-        "{}{}\r\n",
-        status_line,
-        ""
-    );
+    // let full_response = format!(
+    //     "{}{}\r\n",
+    //     status_line,
+    //     ""
+    // );
     // 发送响应头部
-    local_stream.write_all(full_response.as_bytes()).await?;
+    // local_stream.write_all(full_response.as_bytes()).await?;
 
     for header in response_headers.iter() {
 
@@ -451,7 +451,7 @@ pub async fn handle_connection(
             header,
             ""
         );
-        local_stream.write_all(full_response.as_bytes()).await?;
+        local_stream.write_all(head_response.as_bytes()).await?;
 
     }
 
